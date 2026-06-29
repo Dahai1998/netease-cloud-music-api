f838743af44bb2bac739f952eb76f738ba071edd
Dahai1998 <2990471022@qq.com>
2026-06-30 01:37:51 +0800

    Update serveNcmApi to check version on start
    Enable version check during API server startup.

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const path = require('path')
 const tmpPath = require('os').tmpdir()
 
 async function start() {
-  // 检测是否存在 anonymous_token 文件,没有则生成
   if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
     fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
   }
-  // 启动时更新anonymous_token
   const generateConfig = require('./generateConfig')
   await generateConfig()
-
-  const { serveNcmApi } = require('./server')
-  // 关闭版本检测，减少部署网络超时报错
-  serveNcmApi({ checkVersion: false })
+  require('./server').serveNcmApi({
+    checkVersion: true,
+  })
 }
 start()
